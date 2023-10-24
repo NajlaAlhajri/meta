ab0e372964b5f0d2b2a022e0359e03b4c48e68c5
NajlaAlhajri <114076076+NajlaAlhajri@users.noreply.github.com>
2023-10-24 23:28:09 +0300

    Update wrapper_experiments.sh

--- scripts/wrapper_experiments.sh
+++ scripts/wrapper_experiments.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-[ "$(ls -A /home/interactive-mo-ml)" ] || cp -R /Users/najlaalhajri. /home/interactive-mo-ml
+if [ "$(ls -A /home/interactive-mo-ml)" ]; then
+  echo "Directory /home/interactive-mo-ml is not empty."
+else
+  cp -R /Users/najlaalhajri /home/interactive-mo-ml
+fi
+
 cd /home/interactive-mo-ml
 chmod 777 ./scripts/*
 
